fb77d8b16068dc7aa2b334c3754982d6b4ce62c6
osamasy1 <osama.atalla3@gmail.com>
2022-04-07 11:58:37 +0200

    db

--- pdo_toets_herkansing/db/pdo_toets_herkansing (1).sql
+++ pdo_toets_herkansing/db/pdo_toets_herkansing (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 31, 2022 at 12:29 PM
+-- Generation Time: Apr 07, 2022 at 09:57 AM
 -- Server version: 5.7.31
 -- PHP Version: 7.3.21
 
@@ -35,14 +35,16 @@ CREATE TABLE IF NOT EXISTS `burrito` (
   `bonen` varchar(50) NOT NULL,
   `rijst` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `burrito`
 --
 
 INSERT INTO `burrito` (`id`, `burritoformaat`, `saus`, `bonen`, `rijst`) VALUES
-(2, '25', 'Salsa_crudo', 'Kidney Bonen', 'Bruine_rijst');
+(1, '20', 'Salsa_verde', 'Zwarte Bonen', 'Witte_rijst'),
+(2, '20', 'Salsa roja', 'zwarte bonen', 'Witte Rijst'),
+(3, '30', 'Salsa verde', 'kidney bonen', 'Zwarte Rijst');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
